25c9a16113bafbac30a0efabb6b6d7e1dd437cc2
parodez <166378157+parodez@users.noreply.github.com>
2025-04-23 14:32:07 +0800

    changed ui colors

--- ui.py
+++ ui.py
@@ -13,18 +13,19 @@ class display(Tk):
 
         for x in range(10):
             Grid.columnconfigure(self, x, weight=1, uniform='row')
-            Label(self, width = 1, bg = '#FFDBDB').grid(row = 0, column = x, sticky = N+S+E+W)
+            Label(self, width = 1, bg = '#F5EEDD').grid(row = 0, column = x, sticky = N+S+E+W)
         for y in range(10):
             Grid.rowconfigure(self, y, weight=1, uniform='row')
-            Label(self, width = 1, bg = '#FFDBDB').grid(row = y, column = 0, sticky = N+S+E+W)
+            Label(self, width = 1, bg = '#F5EEDD').grid(row = y, column = 0, sticky = N+S+E+W)
 
-        self.configure(background='#FFDBDB')
+        self.configure(background='#F5EEDD')
         self.resizable(False, False)
 
         self.__configurations = self.__widget_configurations()
 
         # Labels
         self.label = self.__configurations['label']
+        self.label_error = self.__configurations['label_error']
         self.label_prime = self.__configurations['label_prime']
         self.label_factorial = self.__configurations['label_factorial']
 
@@ -35,21 +36,31 @@ class display(Tk):
         self.button = self.__configurations['button']
 
     def __widget_configurations(self) -> dict:
+        color1: str = '#F5EEDD'
+        color2: str = '#7AE2CF'
+        color3: str = '#077A7D'
+        color4: str = '#06202B'
+
         # Label configurations
 
         # label
         label: Label = Label(self, text = "Enter Number:", font = ('Arial', 16),
-                      width = 1, height = 1, fg = '#000000', bg = '#FFDBDB')
+                      width = 1, height = 1, fg = '#000000', bg = color1)
         label.grid(row = 1, column = 3, columnspan = 4, rowspan = 1, sticky = N+S+E+W)
+
+        # label_error
+        label_error: Label = Label(self, text = "", font = ('Arial', 10), width = 1,
+                                   height = 1, fg = '#FF0000', bg = color1)
+        label_error.grid(row = 0, column = 2, columnspan = 6, rowspan = 1, sticky = N+S+E+W)
         
         # label_prime
         label_prime: Label = Label(self, text = "", font = ('Arial', 10), width = 1,
-                                   height = 1, fg = '#FFDBDB', bg = '#644A07')
+                                   height = 1, fg = color1, bg = color3)
         label_prime.grid(row = 4, column = 1, columnspan = 8, rowspan = 2, sticky = N+S+E+W)
       
         # label_factorial
         label_factorial: Label = Label(self, text = "", font = ('Arial', 10), width = 1,
-                                       height = 1, fg = '#FFC6C6', bg = '#594100',
+                                       height = 1, fg = color1, bg = color4,
                                        wraplength = 275)
         label_factorial.grid(row = 6, column = 1, columnspan = 8, rowspan = 3, sticky = N+S+E+W)
 
@@ -62,13 +73,14 @@ class display(Tk):
         # Button configurations
         button: Button = Button(self, text = "Enter", font = ('Arial', 16), width = 1,
                                 height = 1, fg = '#000000', 
-                                command = lambda: self.clicked(self.label_prime, self.label_factorial, self.entry_number),
-                                bg = '#FFC6C6')
+                                command = lambda: self.clicked(),
+                                bg = color2)
         button.grid(row = 3, column = 4, columnspan = 2, rowspan = 1, sticky = N+S+E+W)
         
         # Configurations dictionary
         configurations: dict = {
             'label': label,
+            'label_error': label_error,
             'label_prime': label_prime,
             'label_factorial': label_factorial,
             'entry_number': entry_number,
@@ -77,21 +89,21 @@ class display(Tk):
 
         return configurations
         
-    def clicked(self, label_prime: Label, label_factorial: Label, entry_number: Entry):
-        #Checks if value in entry_number is empty or not a digit
-        #Shows a messagebox if either is true
-        if not utils.is_valid(entry_number.get()):
-            messagebox.showinfo(message = "Please input a valid value.")
+    def clicked(self):
+        if not utils.is_valid(self.entry_number.get()):
+            self.label_error.config(text = "Please input a valid value.")
 
         else:
-            str_number: str = entry_number.get()
+            self.label_error.config(text = '')
+            
+            str_number: str = self.entry_number.get()
             int_number: int = int(str_number)
-            
-            if utils.is_prime(int_number):
-                label_prime.config(text = f" {int_number} is a prime number.")
-            else:
-                label_prime.config(text = f" {int_number} is not a prime number.")
-            
-            label_factorial.config(text = f"The factorial of {int_number} is:\n{utils.compute_factorial(int_number)}.")
 
-            entry_number.delete(0, END)
+            if utils.is_prime(int_number):
+                self.label_prime.config(text = f" {int_number} is a prime number.")
+            else:
+                self.label_prime.config(text = f" {int_number} is not a prime number.")
+            
+            self.label_factorial.config(text = f"The factorial of {int_number} is:\n{utils.compute_factorial(int_number)}.")
+
+            self.entry_number.delete(0, END)
